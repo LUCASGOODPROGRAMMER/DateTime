7e53c057c8bcc8af1d10445f9570775eb86a1e0a
LUCASGOODPROGRAMMER <160272134+LUCASGOODPROGRAMMER@users.noreply.github.com>
2024-04-10 12:17:45 -0400

    Add files via upload

---
+++ Ferramentas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDate
+{
+    public static class Ferramentas
+    {
+        public static DateTime SomarDias(string data, int dias)
+        {
+            string dia = data.Substring(0, 2);
+            string mes = data.Substring(3, 2);
+            string ano = data.Substring(6);
+            string dataEUA = $"{ano}/{mes}/{dia}";
+
+            DateTime dataC = Convert.ToDateTime(dataEUA);
+            DateTime resultado = dataC.AddDays(dias);
+            return resultado;
+        }
+        public static TimeSpan DiferencaDias(string primeiraData, string segundaData)
+        {
+            string dia1 = primeiraData.Substring(0, 2);
+            string mes1 = primeiraData.Substring(3, 2);
+            string ano1 = primeiraData.Substring(6);
+            string dataEUA1 = $"{ano1}/{mes1}/{dia1}";
+            DateTime dataB = Convert.ToDateTime(dataEUA1);
+
+            string dia2 = segundaData.Substring(0, 2);
+            string mes2 = segundaData.Substring(3, 2);
+            string ano2 = segundaData.Substring(6);
+            string dataEUA2 = $"{ano2}/{mes2}/{dia2}";
+            DateTime dataC = Convert.ToDateTime(dataEUA2);
+
+            TimeSpan diferenca = dataC.Subtract(dataB);
+            return diferenca;
+        }       
+        public static void Menu()
+        {
+            Console.WriteLine("1-SOMAR DIAS\n2-DIFERENÇA ENTRE DATAS\n");
+            Console.Write("DIGITE A NUMERAÇÃO CORRESPONDENTE A SUA ESCOLHA:(");            
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using BibliotecaDate;
+Ferramentas.Menu();
+string escolha = Console.ReadLine();
+Console.WriteLine("EX(dia/mês/ano-hora:minuto:segundo)");
+if (escolha == "1")
+{
+    Console.Write("Informe a data: ");
+    string data = Console.ReadLine();
+
+    Console.Write("Informe os dias a serem somados: ");
+    int dias = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(Ferramentas.SomarDias(data, dias).ToString("d"));
+}
+else if (escolha == "2")
+{
+    Console.Write("Informe a primeira data: ");
+    string primeiraData = Console.ReadLine();
+
+    Console.Write("Informe a segunda data: ");
+    string segundaData = Console.ReadLine();
+
+    Console.WriteLine(Ferramentas.DiferencaDias(primeiraData, segundaData));
+}
+
+
