785579eeb8da24f529a24fa990d079e89c7d07de
LUCASGOODPROGRAMMER <160272134+LUCASGOODPROGRAMMER@users.noreply.github.com>
2024-04-11 17:28:28 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,33 @@
-﻿using BibliotecaDate;
+using BibliotecaData;
 Ferramentas.Menu();
 string escolha = Console.ReadLine();
-Console.WriteLine("EX(dia/mês/ano-hora:minuto:segundo)");
+
 if (escolha == "1")
 {
+    Console.WriteLine("EX(dia/mês/ano");
     Console.Write("Informe a data: ");
-    string data = Console.ReadLine();
+    string primeiraData = Console.ReadLine();
 
     Console.Write("Informe os dias a serem somados: ");
     int dias = Convert.ToInt32(Console.ReadLine());
 
-    Console.WriteLine(Ferramentas.SomarDias(data, dias).ToString("d"));
+    Console.WriteLine(Ferramentas.SomarDias(primeiraData, dias).ToString("d"));
 }
 else if (escolha == "2")
 {
+    Console.WriteLine("EX(dia/mês/ano hora:minuto:segundo)");
     Console.Write("Informe a primeira data: ");
     string primeiraData = Console.ReadLine();
 
     Console.Write("Informe a segunda data: ");
     string segundaData = Console.ReadLine();
 
-    Console.WriteLine(Ferramentas.DiferencaDias(primeiraData, segundaData));
+    Console.WriteLine("Dias de diferença: " + Ferramentas.DiferencaDias(primeiraData,segundaData).TotalDays);
+    Console.WriteLine("Meses de diferença: " + Ferramentas.DiferencaMeses(primeiraData,segundaData));
+    Console.WriteLine("Anos de diferenca: " + Ferramentas.DiferencaAnos(primeiraData, segundaData));
+
+
 }
+
 
 
